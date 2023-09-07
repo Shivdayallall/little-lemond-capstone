975020248ac467b014fdafba3fc97b900700b092
Shivdayal Lall <shivlall0009@gmail.com>
2023-09-07 11:58:01 -0400

    design and implement the header component

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,11 +1,23 @@
 import "./header.css";
+import heroImg from "../IMG/Hero-image.png";
 
 const Header = () => {
-    return (
-        <header className="header-section">
-            <h1>Little Lemon</h1>
+  return (
+    <header className="header-section">
+      <div className="header-details">
+        <h1>Little Lemon</h1>
+        <h5>Guyana</h5>
+        <p>
+          We are a family own mediterranean resturant, focused on traditional
+          recipes served with a modern twist.
+        </p>
+        <button type="button" class="btn btn-warning reserveBTN">Reserve a table</button>
+      </div>
 
-        </header>
-    )
+      <div className="img-cap-container">
+        <img src={heroImg} alt="man presenting a plate of food" className="img-cap"></img>
+    </div>
+    </header>
+  );
 };
 export default Header;
