da9b02a66b174b180e3fa9a913b46733adf385b7
Shivdayal Lall <shivlall0009@gmail.com>
2023-09-25 10:40:14 -0400

    completed the footer component

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,86 +1,67 @@
 import React from "react";
 import "./footer.css";
-import footerLogo from "../IMG/footerLogo.png"
-import fb from "../IMG/facebook.png"
-import ig from "../IMG/instagram.png"
-import tw from "../IMG/twitter.png"
+import footerLogo from "../IMG/footerLogo.png";
+import footer2 from "../IMG/footer2.png";
+import fb from "../IMG/facebook.png";
+import ig from "../IMG/instagram.png";
+import tw from "../IMG/twitter.png";
 
 const Footer = () => {
   return (
     <footer className="footer-container">
-      <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid">
-          <img className="logo" src={footerLogo} alt="resturant log"></img>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav footer-nav">
-            <h5>Navigation</h5>
-              <li className="nav-item">
-                <a className="nav-link" href="#home">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#about">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#menu">
-                  Menu
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#reservation">
-                  Reservation
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#order-online">
-                  Order Online
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#login">
-                  Login
-                </a>
-              </li>
-            </ul>
-          </div>
+      <div className="footer-nav-container">
+          <img className="footer-img" src={footerLogo} alt="foote"></img>
+        {/* Nav for footer */}
+        <nav className="navLinks">
+          <ul>
+            <li>
+              <a href="home">Home</a>
+            </li>
+            <li>
+              <a href="home">about</a>
+            </li>
+            <li>
+              <a href="home">menu</a>
+            </li>
+            <li>
+              <a href="home">Reservation</a>
+            </li>
+            <li>
+              <a href="home">Order online</a>
+            </li>
+            <li>
+              <a href="home">Login</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="contact-details-container">
+        <div className="contact1">
+          <p>123 Old town rd, Guyana</p>
+          <p>651-789-4561</p>
+          <p>Kay@gmail.com</p>
         </div>
-      </nav>
-      <div className="contact-details">
-            <div className="contact1">
-                <p>123 Buxton rd, Guyana</p>
-                <p>651-789-4561</p>
-                <p>Kay@gmail.com</p>
-            </div>
-            <div className="contact2">
-                <p>123 Broken rd, Guyana</p>
-                <p>651-723-4661</p>
-                <p>Jay@gmail.com</p>
-            </div>
-            <div className="social-icon">
-                <a href="google.com">
-                    <img src={fb} alt="facebook"></img>
-                </a>
-                <a href="google.com">
-                    <img src={ig} alt="instagram"></img>
-                </a>
-                <a href="google.com">
-                    <img src={tw} alt="twitter"></img>
-                </a>
-            </div>
+
+        <div className="contact2">
+          <p>123 New town rd, Guyana</p>
+          <p>651-723-4661</p>
+          <p>Jay@gmail.com</p>
+        </div>
+      </div>
+
+      <div className="social-icons-container">
+        <a href="google.com">
+          <img src={fb} alt="facebook"></img>
+        </a>
+
+        <a href="google.com">
+          <img src={ig} alt="instagram"></img>
+        </a>
+
+        <a href="google.com">
+          <img src={tw} alt="twitter"></img>
+        </a>
       </div>
     </footer>
   );
